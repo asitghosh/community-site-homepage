6f7c874c078f8e7fea3d6b2bde2132b7acde8813
asitghosh <asitghosh.cs@gmail.com>
2018-04-02 15:45:19 +0530

    header-added

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const header = () => {
+	return <div className = "Header" >
+		<p className = "HeaderText">AppDirect Community Site</p>
+		<button className = "SignInButton"> Sign In </button> 
+		</div>
+}
+
+
+export default header;
